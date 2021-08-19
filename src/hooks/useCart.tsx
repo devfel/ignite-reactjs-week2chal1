306a7e684edf2f5ad0f60c03fa8fe1fb944a1e77
Felizardo <devfelizardo@gmail.com>
2021-08-19 02:12:00 -0300

    replacing set item localStorage from item to item to a general useEffect

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
 import { Product, Stock } from "../types";
@@ -32,6 +32,18 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
+  //#1 - Using react to check if there is a change in the cart to update localStorage.
+  const prevCartRef = useRef<Product[]>();
+  useEffect(() => {
+    prevCartRef.current = cart;
+  });
+  const cartPreviousValue = prevCartRef.current ?? cart;
+  useEffect(() => {
+    if (cartPreviousValue !== cart) {
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(cart));
+    }
+  }, [cart, cartPreviousValue]);
+
   const addProduct = async (productId: number) => {
     try {
       const updatedCart = [...cart];
@@ -62,7 +74,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       }
 
       setCart(updatedCart);
-      localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart));
+      // localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart)); //replaced by #1
     } catch {
       toast.error("Erro na adição do produto");
     }
@@ -76,7 +88,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       if (productIndex >= 0) {
         updatedCart.splice(productIndex, 1);
         setCart(updatedCart);
-        localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart));
+        //localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart)); //replaced by #1
       } else {
         throw Error();
       }
@@ -105,7 +117,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       if (productExists) {
         productExists.amount = amount;
         setCart(updatedCart);
-        localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart));
+        //localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart)); //replaced by #1
       } else {
         throw Error();
       }
